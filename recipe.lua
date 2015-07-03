c9e0140208bc9c1638233836525f5e84c40750b0
okkez <okkez000@gmail.com>
2015-07-03 22:34:28 +0900

    spec6: Show specified recipe

--- recipe.lua
+++ recipe.lua
@@ -1,9 +1,17 @@
 #!/usr/bin/lua
 
 filename = arg[1]
+target_id = arg[2]
+
 id = 0
 
 for line in io.lines(filename) do
    id = id + 1
-   print(string.format("%d: %s", id, line))
+   if tonumber(target_id) == id then
+      print(string.format("%d: %s", id, line))
+      os.exit()
+   end
+   if not target_id then
+      print(string.format("%d: %s", id, line))
+   end
 end
